2ed6fd930a3903e89b69903ad8141fed822cbfe4
yaroslav <yaroslav.mim@gmail.com>
2022-04-05 12:33:13 +0300

    https://platform.kata.academy/user/courses/3/1/1/4 change usage from impl to interface

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 
@@ -7,7 +8,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         System.out.println("Created table in given database...");
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,10 @@
 package jm.task.core.jdbc.util;
 
-import org.hibernate.SessionFactory;
 import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 
-import org.hibernate.cfg.Configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
